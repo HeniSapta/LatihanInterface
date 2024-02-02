9b0c5ca5b5eb34286240ea22b56a9f8bf8f2382d
HeniSapta <152345387+HeniSapta@users.noreply.github.com>
2024-02-02 11:06:24 +0700

    Add files via upload

---
+++ TugasP10/TugasP10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TugasPraktikum10
+{
+    abstract class Bunga
+    {
+        public abstract void Voice();
+    }
+
+    class Mawar : Bunga
+    {
+        public override void Voice()
+        {
+            Console.WriteLine("merah");
+        }
+    }
+
+    class Melati : Bunga
+    {
+        public override void Voice()
+        {
+            Console.WriteLine("putih");
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Bunga bunga1 = new Mawar();
+            Bunga bunga2 = new Melati();
+
+            bunga1.Voice();
+            bunga2.Voice();
+        }
+    }
+}
